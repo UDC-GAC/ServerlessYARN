47fd9abe0316b8ac9bf350f36cddb52634fc4b90
Óscar Castellanos Rodríguez <oscar.castellanos@udc.es>
2022-10-17 10:52:08 +0200

    Made easier to load ansible inventory for an existing cluster

--- ansible/provisioning/load_inventory_from_conf.py
+++ ansible/provisioning/load_inventory_from_conf.py
@@ -2,58 +2,73 @@
 import sys
 import io
 import yaml
+from ansible.parsing.dataloader import DataLoader
+from ansible.vars.manager import VariableManager
+from ansible.inventory.manager import InventoryManager
 
 # usage example: load_inventory_from_conf.py /etc/ansible/hosts config/config.yml  -> example outdated
 
-#inventory_file = sys.argv[1]
-#config_file = sys.argv[2]
 inventory_file = "../ansible.inventory"
-config_file = "config/config.yml"
 
+def write_inventory_from_conf(number_of_hosts,number_of_containers_per_node,cpu_per_node,mem_per_node):
 
-with open(config_file, "r") as f:
-    config = yaml.load(f, Loader=yaml.FullLoader)
+    structures = {}
 
-number_of_hosts = config['number_of_client_nodes']
-number_of_containers_per_node = config['number_of_containers_per_node']
+    for i in range(0,number_of_hosts,1):
+        host_name = 'host' + str(i)
+        host_containers = create_container_list(host_name,number_of_containers_per_node)    
+        structures[host_name] = {'containers': host_containers, 'cpu': str(cpu_per_node), 'mem': str(mem_per_node)}
 
-cpu_per_node = config['cpus_per_client_node']
-mem_per_node = config['memory_per_client_node']
+    print(structures)
 
-structures = {}
+    ## delete all previous host lines
+    with open(inventory_file, 'r') as file:
+        # read a list of lines into data
+        data = file.readlines()
 
-for i in range(0,number_of_hosts,1):
+    # write lines until "[nodes]" line (included)
+    with open(inventory_file, "w") as file:
+        i = 0
+
+        while (i < len(data) and data[i] != "[nodes]\n"):
+            file.write(data[i])
+            i+= 1
+        if (i < len(data)):
+            file.write(data[i])
+
+    for host in structures:
+        write_container_list(host,structures[host]['cpu'],structures[host]['mem'],structures[host]['containers'])
+
+def update_inventory_hosts_containers(number_of_containers_per_node):
+
+    loader = DataLoader()
+    ansible_inventory = InventoryManager(loader=loader, sources=inventory_file)
+
+    hostsList = ansible_inventory.groups['nodes'].get_hosts()
+
+    structures = {}
+
+    for host in hostsList:
+        host_name = host.name
+        host_containers = create_container_list(host_name,number_of_containers_per_node)
+        structures[host_name] = {'containers': host_containers, 'cpu': host.vars['cpu'], 'mem': host.vars['mem']}
+
+    print(structures)
+
+    for host in structures:
+        write_container_list(host,structures[host]['cpu'],structures[host]['mem'],structures[host]['containers'])
+
+def create_container_list(host_name,number_of_containers_per_node):
+
     host_containers = []
 
-    host_name = 'host' + str(i)
-
-    for j in range(0,number_of_containers_per_node,1):
-        cont_name = 'cont' + str(j)
+    for i in range(0,number_of_containers_per_node,1):
+        cont_name = 'cont' + str(i)
         host_containers.append(host_name + "-" + cont_name)
 
-    structures[host_name] = {'containers': host_containers, 'cpu': str(cpu_per_node), 'mem': str(mem_per_node)}
+    return host_containers
 
-print(structures)
-
-## delete all previous host lines
-with open(inventory_file, 'r') as file:
-    # read a list of lines into data
-    data = file.readlines()
-
-# write lines until "[nodes]" line (included)
-with open(inventory_file, "w") as file:
-    i = 0
-
-    while (i < len(data) and data[i] != "[nodes]\n"):
-        file.write(data[i])
-        i+= 1
-    if (i < len(data)):
-        file.write(data[i])
-
-## write new info
-for host in structures:
-
-    container_list = structures[host]['containers']
+def write_container_list(host,cpu,mem,container_list):
 
     i = 0
     containers_formatted = "\'["
@@ -82,7 +97,7 @@ for host in structures:
     while (i < len(data) and host not in data[i]):
         i+=1
 
-    host_info = host + " cpu=" + structures[host]['cpu'] + " mem=" + structures[host]['mem'] + " containers=" + containers_formatted + "\n"
+    host_info = host + " cpu=" + str(cpu) + " mem=" + str(mem) + " containers=" + containers_formatted + "\n"
 
     if (i < len(data)):
         data[i] = host_info
@@ -92,3 +107,25 @@ for host in structures:
         new_line = host_info
         with open(inventory_file, 'a') as file:
             file.writelines( host_info )
+
+if __name__ == "__main__":   
+
+    #inventory_file = sys.argv[1]
+    #config_file = sys.argv[2]
+    config_file = "config/config.yml"
+
+    with open(config_file, "r") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    number_of_hosts = config['number_of_client_nodes']
+    number_of_containers_per_node = config['number_of_containers_per_node']
+
+    cpu_per_node = config['cpus_per_client_node']
+    mem_per_node = config['memory_per_client_node']
+
+    virtual_mode = config['virtual_mode']
+
+    if (virtual_mode): 
+        write_inventory_from_conf(number_of_hosts,number_of_containers_per_node,cpu_per_node,mem_per_node)
+    else: 
+        update_inventory_hosts_containers(number_of_containers_per_node)
