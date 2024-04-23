351aba2111f18d5a965aa992f909552375e9335a
TomeMD <tome.maseda@udc.es>
2024-04-23 14:45:50 +0200

    Modified WattWizard launch using redesigned implementation

--- ansible/provisioning/templates/wattwizard/global_config.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-confDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
-
-export TRAIN_FILE_NAME="{{ train_file_name }}"
-export TRAIN_FILE=${confDir}/"{{ train_file_name }}".timestamps
-export MODEL_VARIABLES="{{ model_variables }}"
-export PREDICTION_METHODS="{{ prediction_methods }}"
-export INFLUXDB_BUCKET="{{ influxdb_bucket }}"
--- ansible/provisioning/templates/wattwizard/influxdb_config.py
+++
@@ -1,3 +0,0 @@
-INFLUXDB_URL = "http://{{ influxdb_host }}:8086"
-INFLUXDB_TOKEN = "{{ influxdb_token }}"
-INFLUXDB_ORG = "{{ influxdb_org }}"
