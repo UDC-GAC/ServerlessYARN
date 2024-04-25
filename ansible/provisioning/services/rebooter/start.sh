a6fa4fc76ca19ce4893e0ea761b56da07b75c9c2
Óscar Castellanos Rodríguez <oscar.castellanos@udc.es>
2024-04-25 14:17:26 +0200

    Allow Rebooter to actually reboot services on older versions of tmux

--- ansible/provisioning/services/rebooter/start.sh
+++ ansible/provisioning/services/rebooter/start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 
+## Unset TMUX variable to allow start tmux sessions inside the Rebooter sessions in older versions of tmux
+unset TMUX
 python3 ${scriptDir}/Rebooter.py
