88151fac6d1025832883c8b42f9767020841604a
Óscar Castellanos Rodríguez <oscar.castellanos@udc.es>
2024-04-30 14:40:57 +0200

    Fixes to LV management, especially when deploying on an existing cluster. Also allows to easily remove created volumes

--- ansible/provisioning/scripts/stop_all.sh
+++ ansible/provisioning/scripts/stop_all.sh
@@ -5,6 +5,9 @@ scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 
 INVENTORY=${scriptDir}/../../ansible.inventory
 
+## If you want to remove or keep the logical volumes created (if any)
+remove_lv=no
+
 # This is useful in case we need to use a newer version of ansible installed in $HOME/.local/bin
 export PATH=$HOME/.local/bin:$PATH
 
@@ -12,3 +15,12 @@ echo ""
 echo "Stopping all services..."
 ansible-playbook ${scriptDir}/../stop_services_playbook.yml -i $INVENTORY -t stop_services
 echo "Stop Done!"
+
+
+if [ "$remove_lv" = yes ];
+then
+    echo ""
+    echo "Removing logical volumes..."
+    ansible-playbook ${scriptDir}/../install_playbook.yml -i $INVENTORY -t remove_lv
+    echo "Logical Volumes deleted!"
+fi
