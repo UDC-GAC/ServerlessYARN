e087b34bfd1da11fc96ab324c7f87a70e9143961
TomeMD <tome.maseda@udc.es>
2024-08-26 11:37:08 +0200

    Added new application to run NPB kernels on containers

---
+++ ansible/provisioning/apps/npb_app/install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+export SCRIPT_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+export FILES_DIR="${SCRIPT_DIR}/files_dir"
+export DEBIAN_FRONTEND=noninteractive
+
+. "${FILES_DIR}/get_env.sh"
+
+apt-get -y update
+apt-get -y install build-essential wget gfortran libopenmpi-dev openmpi-bin
+
+# Download NPB
+wget "${NPB_DOWNLOAD_URL}"
+tar -xf "${NPB_TAR_FILE}" -C "${NPB_INSTALL_DIR}"
+rm "${NPB_TAR_FILE}"
+
+# Compile NPB OMP kernels
+cd "${NPB_OMP_HOME}"
+cp config/make.def.template config/make.def
+
+# Add -mcmodel=medium to fix error "relocation truncated to fit" for IS kernel
+sed -i 's/^CFLAGS\t=.*/CFLAGS\t= -O3 -fopenmp -mcmodel=medium/' config/make.def
+
+make clean
+for KERNEL in "${NPB_KERNELS[@]}";do
+    make "${KERNEL}" CLASS="${NPB_CLASS}"
+done
---
+++ ansible/provisioning/apps/npb_app/start.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+export SCRIPT_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+export FILES_DIR="${SCRIPT_DIR}/files_dir"
+
+. "${FILES_DIR}/get_env.sh"
+
+# Clean/Create output directory
+rm -rf "${NPB_OUTPUT_DIR}/*"
+mkdir -p "${NPB_OUTPUT_DIR}"
+
+echo "Starting kernels execution"
+for KERNEL in "${NPB_KERNELS[@]}";do
+  # Set number of threads
+  export OMP_NUM_THREADS="${NUM_THREADS}"
+
+  echo "[$(date -u "+%Y-%m-%d %H:%M:%S%z")] Running kernel ${KERNEL} (class=${NPB_CLASS}) with ${NUM_THREADS} threads" | tee -a "${NPB_OUTPUT_DIR}/results.log"
+  # Run kernel
+  START_TEST=$(date +%s%N)
+  ${NPB_OMP_HOME}/bin/${KERNEL}.${NPB_CLASS}.x >> "${NPB_OUTPUT_DIR}/${KERNEL}-output.log" 2>&1
+  END_TEST=$(date +%s%N)
+  EXECUTION_TIME=$(bc <<< "scale=9; $(( END_TEST - START_TEST )) / 1000000000")
+
+  # Log results
+  echo "[$(date -u "+%Y-%m-%d %H:%M:%S%z")] Execution time for kernel ${KERNEL} (class=${NPB_CLASS}) with ${NUM_THREADS} thread(s): ${EXECUTION_TIME}" | tee -a "${NPB_OUTPUT_DIR}/results.log"
+
+  # Sleep 5 minutes between executions
+  sleep 300
+done
+
+# Grant permissions to access results outside the container
+chmod -R 777 "${NPB_OUTPUT_DIR}"
