ec95131703b2707d238a64fa80b4fc94baab97c8
TomeMD <tome.maseda@udc.es>
2024-10-30 14:09:25 +0100

    Update NPB tests

---
+++ ansible/provisioning/tests/npb/bin/run-app.py
@@ -0,0 +1,118 @@
+import sys
+import time
+import redis
+import requests
+from urllib.parse import urljoin
+from datetime import datetime, timezone
+
+WEB_INTERFACE_URL = "http://localhost:9000"
+ORCHESTRATOR_URL = "http://localhost:5000"
+NUMBER_OF_CONTAINERS = "1"
+ASSIGNATION_POLICY = "Best-effort"
+BENEVOLENCE = "3"  # 1: "Lax", 2: "Medium", 3: "Strict"
+POLLING_FREQUENCY = 5
+
+
+def get_csrf_token(session):
+    # Do some random request to get CSRF token
+    response = session.get(urljoin(WEB_INTERFACE_URL, "/ui/apps"))
+
+    if response.status_code != 200:
+        raise Exception("Error doing GET request in order to get CSRF token")
+
+    # Extract CSRF token from cookies
+    for cookie in session.cookies:
+        if cookie.name == 'csrftoken':
+            return cookie.value
+
+    raise Exception("CSRF token not found in cookies")
+
+
+def start_app(session, app_name):
+
+    # Get CSRF token
+    csrf_token = get_csrf_token(session)
+
+    # Set headers
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "X-CSRFToken": csrf_token,
+    }
+
+    # Set data
+    data = {
+        "csrfmiddlewaretoken": csrf_token,
+        "operation": "add",
+        "structure_type": "containers_to_app",
+        "name": app_name,
+        "number_of_containers": NUMBER_OF_CONTAINERS,
+        "assignation_policy": ASSIGNATION_POLICY,
+        "benevolence": BENEVOLENCE,
+        "save": "Start App",
+    }
+
+    # Start app through web interface
+    response = session.post(urljoin(WEB_INTERFACE_URL, "/ui/apps"), headers=headers, data=data)
+
+    if response.status_code != 200:
+        raise Exception(f"Failed to start app through web interface: {response}")
+
+
+def get_app_redis_key(redis_server, app_name):
+    for key in redis_server.scan_iter("{0}:*".format("pending_tasks")):
+        task_name = redis_server.hget(key, "task_name").decode("utf-8")
+        if task_name.startswith(app_name):
+            return key
+    raise Exception(f"Redis key for app {app_name} not found")
+
+
+def get_containers_from_app(session, app_name):
+    response = session.get(urljoin(ORCHESTRATOR_URL, f"/structure/{app_name}"))
+    return response.json()['containers']
+
+
+def update_containers_file(file, containers):
+    current_time = datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S%z')
+    with open(file, 'a') as f:
+        for container in containers:
+            f.write(f"{current_time} {container}\n")
+
+
+def wait_for_app_to_finish(session, redis_server, app_name):
+    app_running = True
+    app_key = get_app_redis_key(redis_server, app_name)
+    while app_running:
+        # Run a GET request to the web interface to force the update of tasks status in redis
+        response = session.get(urljoin(WEB_INTERFACE_URL, "/ui/apps"))
+        try:
+            task = redis_server.hget(app_key, "task_name")
+            if not task:
+                app_running = False
+        except Exception as e:
+            app_running = False
+            print(str(e))
+
+        time.sleep(POLLING_FREQUENCY)
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 3:
+        print("At least 1 argument is needed")
+        print("1 -> app name (e.g., npb_app)")
+        print("2 -> containers file (e.g., ./out/containers)")
+        sys.exit(0)
+
+    app_name = sys.argv[1]
+    containers_file = sys.argv[2]
+
+    web_interface_session = requests.Session()
+    orchestrator_session = requests.Session()
+    redis_server = redis.StrictRedis()
+
+    start_app(web_interface_session, app_name)
+    time.sleep(60)  # Wait some time for the app to be subscribed to SC
+    containers = get_containers_from_app(orchestrator_session, app_name)
+    update_containers_file(containers_file, containers)
+
+    wait_for_app_to_finish(web_interface_session, redis_server, app_name)
--- ansible/provisioning/tests/npb/run-tests.sh
+++ ansible/provisioning/tests/npb/run-tests.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
 #########################################################################################################
-# CHANGE EXPERIMENT HERE!!
+# EXPERIMENTS
 #########################################################################################################
+# Current experiment
 EXPERIMENT="1cont_1thread"
 
+# TODO: Run all experiments in a loop -> App should be removed when a whole experiment finishes and another starts
 # Experiments configs
 declare -A EXPERIMENT_CONFIG
 EXPERIMENT_CONFIG["1cont_1thread"]="1,polyreg_Single_Core,sgdregressor_Single_Core,multisocket_hw_aware"
 EXPERIMENT_CONFIG["1cont_32thread"]="32,polyreg_General,sgdregressor_General,multisocket_hw_aware"
 EXPERIMENT_CONFIG["1cont_64thread"]="64,polyreg_General,sgdregressor_General,multisocket_hw_aware"
 
-# Read config for current experiment
-IFS="," read NPB_NUM_THREADS STATIC_POWER_MODEL DYNAMIC_POWER_MODEL HW_AWARE_POWER_MODEL <<< "${EXPERIMENT_CONFIG[${EXPERIMENT}]}"
-DYNAMIC_SHARES_PER_WATT=28 # cpu_max_shares / (max_energy - min_energy)
-
-# Directories
+#########################################################################################################
+# ENVIRONMENT
+#########################################################################################################
+APP_NAME="npb_app"
 SCRIPT_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 BIN_DIR="${SCRIPT_DIR}/bin"
 CONF_DIR="${SCRIPT_DIR}/etc"
@@ -26,96 +27,102 @@ PROVISIONING_DIR=$(dirname "${TEST_DIR}")
 ANSIBLE_DIR=$(dirname "${PROVISIONING_DIR}")
 ANSIBLE_INVENTORY="${ANSIBLE_DIR}/ansible.inventory"
 
-# Host information
-IFS='.' read -ra HOST_INFO <<< $(hostname)
-IFS='-' read -ra HOST_PARTS <<< "${HOST_INFO}"
-REMOTE_HOST_NUM=$(( HOST_PARTS[2] + 1))
-REMOTE_HOST="${HOST_PARTS[0]}-${HOST_PARTS[1]}-${REMOTE_HOST_NUM}"
-CONTAINER_NAME="${REMOTE_HOST}-cont0"
-
+#########################################################################################################
+# AUXILIAR FUNCTIONS
+#########################################################################################################
 function log_timestamp() {
   local EXPERIMENT_NAME="${1}"
   local LABEL="${2}"
   echo "${EXPERIMENT_NAME} ${LABEL} $(date -u "+%Y-%m-%d %H:%M:%S%z")" | tee -a "${OUTPUT_DIR}/experiments.log"
 }
 
-function run_npb() {
+function run_app() {
   local EXPERIMENT_NAME="${1}"
   sleep 20
   log_timestamp "${EXPERIMENT_NAME}" "start"
-  unbuffer ansible-playbook ${BIN_DIR}/run_npb.yml -i ${ANSIBLE_INVENTORY} -t start_app --extra-vars "num_threads=${NPB_NUM_THREADS}"
+  python3 "${BIN_DIR}/run-app.py" "${APP_NAME}" "${OUTPUT_DIR}/containers"
   log_timestamp "${EXPERIMENT_NAME}" "stop"
-  sleep 60
-  bash "${BIN_DIR}/reset-container-limits.sh" "${CONTAINER_NAME}" "${REMOTE_HOST}"
   sleep 60
 }
 
+function change_npb_num_threads() {
+  sed -i "s/^\(export NUM_THREADS=\)[0-9]\+$/\1${1}/" "${PROVISIONING_DIR}/apps/${APP_NAME}/files_dir/get_env.sh"
+}
+
+#########################################################################################################
+# INITIALIZATION
+#########################################################################################################
 # Create output and images directories
 echo "Output directory: ${OUTPUT_DIR}"
 echo "Images directory: ${IMG_DIR}"
 mkdir -p "${OUTPUT_DIR}" "${IMG_DIR}"
 
-echo "Using ${CONF_DIR}/${EXPERIMENT}.yml to set ServerlessYARN configuration"
-cp "${CONF_DIR}/${EXPERIMENT}.yml" "${CONF_DIR}/config.yml"
+echo "Using ${CONF_DIR}/config.yml to set ServerlessYARN configuration"
 cp "${CONF_DIR}/config.yml" "${PROVISIONING_DIR}/config/config.yml"
 
-echo "Running ServerlessYARN"
+echo "Using ${CONF_DIR}/${EXPERIMENT}.yml to set NPB application configuration"
+cp "${CONF_DIR}/${EXPERIMENT}.yml" "${PROVISIONING_DIR}/apps/${APP_NAME}/app_config.yml"
+
+echo "Launch ServerlessYARN"
 bash "${PROVISIONING_DIR}/scripts/start_all.sh"
-sleep 600
 
-echo "Stopping Rebooter before tests"
-bash "${BIN_DIR}/stop-rebooter.sh"
-
-echo "Start and deactivate WattTrainer"
-bash "${BIN_DIR}/start-watt-trainer.sh"
+echo "Ensure WattTrainer is deactivated"
 bash "${BIN_DIR}/deactivate-watt-trainer.sh"
 
-echo "Running NPB tests in ${CONTAINER_NAME}"
+# Let the platform 5 minutes to stabilise
+sleep 300
+
+# Read config for current experiment
+IFS="," read NPB_NUM_THREADS STATIC_POWER_MODEL DYNAMIC_POWER_MODEL HW_AWARE_POWER_MODEL <<< "${EXPERIMENT_CONFIG[${EXPERIMENT}]}"
+DYNAMIC_SHARES_PER_WATT=28 # cpu_max_shares / (max_energy - min_energy)
+change_npb_num_threads "${NPB_NUM_THREADS}"
+
+echo "Running tests for app ${APP_NAME}"
 #########################################################################################################
 # calibration: One execution first just to calibrate SmartWatts
 #########################################################################################################
 bash "${BIN_DIR}/deactivate-serverless.sh"
-run_npb "calibration"
+run_app "calibration"
 
 #########################################################################################################
 # no_serverless: Run NPB without ServerlessContainers
 #########################################################################################################
 bash "${BIN_DIR}/deactivate-serverless.sh"
-run_npb "no_serverless"
+run_app "no_serverless"
 
 #########################################################################################################
 # serverless_fixed_value: Run NPB with ServerlessContainers using a fixed shares per watt value
 #########################################################################################################
 bash "${BIN_DIR}/activate-serverless.sh"
 bash "${BIN_DIR}/change-energy-rules-policy.sh" "fixed-ratio"
-run_npb "serverless_fixed_value"
+run_app "serverless_fixed_value"
 
 #########################################################################################################
 # serverless_dynamic_value: Run NPB with ServerlessContainers using a custom shares per watt value
 #########################################################################################################
 bash "${BIN_DIR}/change-shares-per-watt.sh" "${DYNAMIC_SHARES_PER_WATT}"
 bash "${BIN_DIR}/change-energy-rules-policy.sh" "fixed-ratio"
-run_npb "serverless_dynamic_value"
+run_app "serverless_dynamic_value"
 
 #########################################################################################################
 # proportional_scaling: Run NPB with ServerlessContainers using CPU/energy proportional rescaling
 #########################################################################################################
 bash "${BIN_DIR}/change-energy-rules-policy.sh" "proportional"
-run_npb "proportional_scaling"
+run_app "proportional_scaling"
 
 #########################################################################################################
 # serverless_static_model: Run NPB with ServerlessContainers using power modelling
 #########################################################################################################
 bash "${BIN_DIR}/change-energy-rules-policy.sh" "modelling"
 bash "${BIN_DIR}/change-model.sh" "${STATIC_POWER_MODEL}"
-run_npb "serverless_static_model"
+run_app "serverless_static_model"
 
 #########################################################################################################
 # hw_aware_model: Run NPB with ServerlessContainers using HW aware power modelling
 #########################################################################################################
 bash "${BIN_DIR}/change-energy-rules-policy.sh" "modelling"
 bash "${BIN_DIR}/change-model.sh" "${HW_AWARE_POWER_MODEL}"
-run_npb "hw_aware_model"
+run_app "hw_aware_model"
 
 #########################################################################################################
 # serverless_dynamic_model: Run NPB with ServerlessContainers using power modelling and online learning
@@ -123,9 +130,10 @@ run_npb "hw_aware_model"
 bash "${BIN_DIR}/activate-watt-trainer.sh"
 bash "${BIN_DIR}/change-energy-rules-policy.sh" "modelling"
 bash "${BIN_DIR}/change-model.sh" "${DYNAMIC_POWER_MODEL}"
-run_npb "serverless_dynamic_model"
+run_app "serverless_dynamic_model"
 
 sleep 30
-python3 "${BIN_DIR}/get_metrics_opentsdb.py" "${OUTPUT_DIR}/experiments.log" "${IMG_DIR}" "${CONTAINER_NAME}"
+python3 "${BIN_DIR}/get-metrics-opentsdb.py" "${OUTPUT_DIR}/experiments.log" "${OUTPUT_DIR}/containers" "${IMG_DIR}"
 
+# Keep SLURM job active just in case an error has occurred during execution
 sleep 259200
