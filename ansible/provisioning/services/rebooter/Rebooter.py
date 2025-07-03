0559efd5b89aed734c09f362c91150cd671e00df
Óscar Castellanos Rodríguez <oscar.castellanos@udc.es>
2025-07-03 13:01:59 +0200

    Added watchdog to Rebooter service to monitor changes in config module files and automatically update the main config file

--- ansible/provisioning/services/rebooter/Rebooter.py
+++ ansible/provisioning/services/rebooter/Rebooter.py
@@ -10,18 +10,39 @@ from ansible_runner import Runner, RunnerConfig
 import yaml
 import requests
 import json
+import os
+
+from watchdog.observers.polling import PollingObserver
+from watchdog.events import FileSystemEventHandler
 
 SERVICE_NAME = "rebooter"
 BDW_SERVICES = ["EVE_TIMES", "OPENTSDB"]
 SC_SERVICES = ["orchestrator", "database_snapshoter", "structure_snapshoter", "guardian", "scaler", "refeeder", "sanity_checker", "rebalancer"]
-ASW_SERVICES = ["web_interface", "celery", "redis_server"]
+SY_SERVICES = ["web_interface", "celery", "redis_server"]
 
-SERVICES = BDW_SERVICES + SC_SERVICES + ASW_SERVICES
+SERVICES = BDW_SERVICES + SC_SERVICES + SY_SERVICES
 
 ONLY_VIRTUAL_MODE = ["EVE_TIMES"]
 
-private_data_dir = "../.."
+scriptDir = os.path.realpath(os.path.dirname(__file__))
+playbook_dir = scriptDir + "/../.."
 debug = True
+
+## Update config file
+class EventHandler(FileSystemEventHandler):
+    def on_any_event(self, event):
+        if event.src_path.endswith(".yml"):  # Only process files ending in .yml, excluding .template files
+            log_info(event, debug)
+            ## Run the playbook to update the config file
+            rc = RunnerConfig(
+                private_data_dir=playbook_dir,
+                playbook="load_config_playbook.yml",
+                inventory='../ansible.inventory'
+            )
+            rc.prepare()
+            r = Runner(config=rc)
+            r.run()
+            log_info("Updated config file to due modification on modules", debug)
 
 ## Logging
 def log_info(message, debug):
@@ -45,7 +66,7 @@ def get_time_now_string():
 def stop_opentsdb():
     ## Stop OpenTSDB
     rc = RunnerConfig(
-        private_data_dir=private_data_dir,
+        private_data_dir=playbook_dir,
         playbook="stop_services_playbook.yml",
         tags='stop_opentsdb',
         inventory='../ansible.inventory'
@@ -84,7 +105,7 @@ def check_services():
     global SERVICES
 
     ## Test connection for OPENTSDB setup
-    config_file = private_data_dir + "/config/config.yml"
+    config_file = scriptDir + "/../../config/config.yml"
     with open(config_file, "r") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
 
@@ -98,52 +119,64 @@ def check_services():
 
     ## Playbook running setup
     rc = RunnerConfig(
-        private_data_dir=private_data_dir,
+        private_data_dir=playbook_dir,
         playbook="launch_playbook.yml",
         tags='restart_services',
         inventory='../ansible.inventory'
     )
-
     rc.prepare()
     r = Runner(config=rc)
 
-    while True:
+    ## Setup watchdog to monitor config file changes
+    event_handler = EventHandler()
+    observer = PollingObserver()
+    observer.schedule(event_handler, scriptDir + "/../../config/modules/", recursive=True)
+    observer.start()
+    log_info("Monitoring config changes on {0}".format(scriptDir + "/../../config/modules/"), debug)
 
-        server = libtmux.Server()
-        sessions_missing = 0
+    try:
+        while True:
 
-        try:
-            ## OpenTSBD test
-            test_opentsdb_connection(opentsdb_server)
+            server = libtmux.Server()
+            sessions_missing = 0
 
-            for service in SERVICES:
-                service_session = server.find_where({ "session_name": service })
-                if not service_session:
-                    sessions_missing += 1
-                    log_warning("{0} session missing".format(service),debug)
+            try:
+                ## OpenTSBD test
+                test_opentsdb_connection(opentsdb_server)
 
-        except libtmux.exc.LibTmuxException:
-            sessions_missing += 1
-            log_warning("No service started",debug)
+                for service in SERVICES:
+                    service_session = server.find_where({ "session_name": service })
+                    if not service_session:
+                        sessions_missing += 1
+                        log_warning("{0} session missing".format(service),debug)
 
-        if sessions_missing:
-            ## restart services
-            r.run()
-            log_info("{},{}: {}".format(r.status, r.rc, r.stderr),debug)
+            except libtmux.exc.LibTmuxException:
+                sessions_missing += 1
+                log_warning("No service started",debug)
 
-        else:
-            log_info("All services started", debug)
+            if sessions_missing:
+                ## restart services
+                r.run()
+                log_info("{},{}: {}".format(r.status, r.rc, r.stderr),debug)
 
-        log_info("Services checked", debug)
-        log_info("-------------------------------------", debug)
+            else:
+                log_info("All services started", debug)
 
-        delay = 300
-        time_waited = 0
-        heartbeat_delay = 60  # seconds
+            log_info("Services checked", debug)
+            log_info("-------------------------------------", debug)
 
-        while time_waited < delay:
-            time.sleep(heartbeat_delay)
-            time_waited += heartbeat_delay
+            delay = 300
+            time_waited = 0
+            heartbeat_delay = 60  # seconds
+
+            while time_waited < delay:
+                time.sleep(heartbeat_delay)
+                time_waited += heartbeat_delay
+
+    except KeyboardInterrupt:
+        observer.stop()
+        log_warning(SERVICE_NAME.capitalize() + " stopped by user", debug)
+    observer.join()
 
 def main():
     try:
