620f37a9aebfbb81bb3a681da914f32963972e2c
Óscar Castellanos Rodríguez <oscar.castellanos@udc.es>
2025-07-09 14:08:17 +0200

    Implemented the 'output_dir' parameter to store app output data in the server node. Updated the sample app to use the new parameter and changed the timezone in the Django project to be consistent with that of nodes

--- ansible/provisioning/apps/sample_app/start.sh
+++ ansible/provisioning/apps/sample_app/start.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 
-source ${scriptDir}/runtime_files/app_config.sh
+source ${scriptDir}/{{ runtime_files }}/app_config.sh
+
+OUTPUT_DIR="{{ bind_dir_on_container }}/{{ output_dir }}"
+mkdir $OUTPUT_DIR
 
 # Check if stress is available
 path_to_executable=$(which stress)
 if [ -x "$path_to_executable" ] ; then
     echo "$path_to_executable available"
-    ${path_to_executable} --cpu $CORES_TO_STRESS --timeout $TIMEOUT_SECONDS
+    ${path_to_executable} --cpu $CORES_TO_STRESS --timeout $TIMEOUT_SECONDS >> $OUTPUT_DIR/stress_output.txt
 else
-    echo "Stress not available, using simple stresser..."
-    seq $CORES_TO_STRESS | xargs -P0 -n1 timeout $TIMEOUT_SECONDS md5sum /dev/zero
+    echo "Stress not available, using simple stressor..."
+    (time seq $CORES_TO_STRESS | xargs -P0 -n1 timeout $TIMEOUT_SECONDS md5sum /dev/zero) &>> $OUTPUT_DIR/simple_stressor.txt
 fi
 
 exit_code=$?
