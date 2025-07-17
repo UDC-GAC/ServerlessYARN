3ce7c3148a1837a5ef56b60802fe13ac766bdd91
Óscar Castellanos Rodríguez <oscar.castellanos@udc.es>
2025-07-17 11:07:54 +0200

    Sample app now shows an example of 'install_files' usage

--- ansible/provisioning/apps/sample_app/install.sh
+++ ansible/provisioning/apps/sample_app/install.sh
@@ -2,4 +2,16 @@
 ## Optional file if you need to install some packages on the containers
 
 apt-get -y update
-apt-get -y install stress
+
+if [ -d /opt/{{ app_dir | basename }}/{{ install_files | basename  }} ] && [ -f /opt/{{ app_dir | basename }}/{{ install_files | basename  }}/package_config.sh ];
+then
+    # Install other package
+    source /opt/{{ app_dir | basename }}/{{ install_files | basename }}/package_config.sh
+    if [ $INSTALL_STRESS_NG = 1 ];
+    then
+        apt-get -y install stress-ng
+    fi
+else
+    # Install stress
+    apt-get -y install stress
+fi
--- ansible/provisioning/apps/sample_app/start.sh
+++ ansible/provisioning/apps/sample_app/start.sh
@@ -6,11 +6,21 @@ source ${scriptDir}/{{ runtime_files }}/app_config.sh
 OUTPUT_DIR="{{ bind_dir_on_container }}/{{ output_dir }}"
 #mkdir -p $OUTPUT_DIR # --> it is not necessary to create it, it will be done by an Ansible task
 
+stress_ng_executable=$(which stress-ng)
+stress_executable=$(which stress)
+
+# Check if stress-ng is available
+if [ -x "$stress_ng_executable" ] ; then
+    echo "$stress_ng_executable available"
+    echo "Using `stress-ng --version`" >> $OUTPUT_DIR/stress_ng_output.txt
+    ${stress_ng_executable} --cpu $CORES_TO_STRESS --timeout $TIMEOUT_SECONDS &>> $OUTPUT_DIR/stress_ng_output.txt
+
 # Check if stress is available
-path_to_executable=$(which stress)
-if [ -x "$path_to_executable" ] ; then
-    echo "$path_to_executable available"
-    ${path_to_executable} --cpu $CORES_TO_STRESS --timeout $TIMEOUT_SECONDS >> $OUTPUT_DIR/stress_output.txt
+elif [ -x "$stress_executable" ] ; then
+    echo "$stress_executable available"
+    echo "Using `stress --version` version" >> $OUTPUT_DIR/stress_output.txt
+    ${stress_executable} --cpu $CORES_TO_STRESS --timeout $TIMEOUT_SECONDS >> $OUTPUT_DIR/stress_output.txt
+
 else
     echo "Stress not available, using simple stressor..."
     (time seq $CORES_TO_STRESS | xargs -P0 -n1 timeout $TIMEOUT_SECONDS md5sum /dev/zero) &>> $OUTPUT_DIR/simple_stressor.txt
