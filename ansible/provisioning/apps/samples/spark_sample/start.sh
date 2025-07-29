fdb777ae0e6c2018966e83442c5c9e6145c67d3e
Óscar Castellanos Rodríguez <oscar.castellanos@udc.es>
2025-07-29 13:45:01 +0200

    Updated spark_sample app and added an app_config.yml file

--- ansible/provisioning/apps/samples/spark_sample/start.sh
+++ ansible/provisioning/apps/samples/spark_sample/start.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 cd {{ bind_dir_on_container }}
+set -e
+
+OUTPUT_DIR="{{ bind_dir_on_container }}/{{ output_dir }}"
+
+output_file="$OUTPUT_DIR/output_spark_app_`date +%H-%M-%S`"
+app_log_file="$OUTPUT_DIR/app_log_spark_app_`date +%H-%M-%S`"
+runtime_file="$OUTPUT_DIR/runtime_spark_app_`date +%H-%M-%S`"
 
 # Run JAR
 start=`date +%s.%N`
 $SPARK_HOME/bin/spark-submit \
   --class org.apache.spark.examples.SparkPi \
   --master yarn \
-  --deploy-mode cluster \
+  --deploy-mode client \
   $SPARK_HOME/examples/jars/spark-examples_2.12-{{ spark_version }}.jar \
-  1000
+  1000 2> $app_log_file 1>$output_file
+
 end=`date +%s.%N`
 jar_runtime=$( echo "$end - $start" | bc -l )
 
 # Store runtimes
-echo Spark JAR runtime: $jar_runtime > runtime
+echo Spark JAR runtime: $jar_runtime > $runtime_file
 
