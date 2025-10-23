fc0ba830b292330c142e8df94678b74340f0efac
Óscar Castellanos Rodríguez <oscar.castellanos@udc.es>
2025-10-23 21:18:08 +0200

    Update HDFS sample app to use the non-root user

--- ansible/provisioning/apps/samples/hdfs_sample/start.sh
+++ ansible/provisioning/apps/samples/hdfs_sample/start.sh
@@ -2,13 +2,13 @@
 cd {{ bind_dir_on_container }}
 set -e
 
-$HADOOP_HOME/bin/hdfs dfs -mkdir -p /user/root
+$HADOOP_HOME/bin/hdfs dfs -mkdir -p /user/{{ user_info.user_name }}
 
 ## Download data from local HDFS (should have been previously downloaded from global HDFS)
-$HADOOP_HOME/bin/hdfs dfs -get /user/root/input_data $HOME/input_data
+$HADOOP_HOME/bin/hdfs dfs -get /user/{{ user_info.user_name }}/input_data {{ user_home_on_container }}/input_data
 
 ## Modify downloaded data
-echo "Test to upload data in HDFS: local --> global" >> $HOME/input_data
+echo "Test to upload data in HDFS: local --> global" >> {{ user_home_on_container }}/input_data
 
 ## Upload data to local HDFS (should be uploaded afterwards to global HDFS to persist it)
-$HADOOP_HOME/bin/hdfs dfs -put $HOME/input_data /user/root/output_data
+$HADOOP_HOME/bin/hdfs dfs -put {{ user_home_on_container }}/input_data /user/{{ user_info.user_name }}/output_data
