6ad4a0f9510635b542dac06b2db349d394f44b08
Óscar Castellanos Rodríguez <oscar.castellanos@udc.es>
2025-11-05 20:35:14 +0100

    Updated HDFS sample app to show example of global data direct transfer

--- ansible/provisioning/apps/samples/hdfs_sample/start.sh
+++ ansible/provisioning/apps/samples/hdfs_sample/start.sh
@@ -2,13 +2,19 @@
 cd {{ bind_dir_on_container }}
 set -e
 
-$HADOOP_HOME/bin/hdfs dfs -mkdir -p /user/{{ user_info.user_name }}
+DIRECT_TRANSFER=1  # transfer data from/to global HDFS directly or through the local HDFS as an intermediary
 
-## Download data from local HDFS (should have been previously downloaded from global HDFS)
-$HADOOP_HOME/bin/hdfs dfs -get /user/{{ user_info.user_name }}/input_data {{ user_home_on_container }}/input_data
+if [ $DIRECT_TRANSFER == 1 ]
+then
+    GLOBAL_NAMENODE_IP=$(getent hosts {{ namenode_container_name }} | awk '{print $1 }' | head -n 1)
+    INPUT_DATA_PREFIX=hdfs://$GLOBAL_NAMENODE_IP
+fi
+
+## Download data
+$HADOOP_HOME/bin/hdfs dfs -get $INPUT_DATA_PREFIX/user/{{ user_info.user_name }}/input_data {{ user_home_on_container }}/input_data
 
 ## Modify downloaded data
 echo "Test to upload data in HDFS: local --> global" >> {{ user_home_on_container }}/input_data
 
-## Upload data to local HDFS (should be uploaded afterwards to global HDFS to persist it)
-$HADOOP_HOME/bin/hdfs dfs -put {{ user_home_on_container }}/input_data /user/{{ user_info.user_name }}/output_data
+## Upload data
+$HADOOP_HOME/bin/hdfs dfs -put {{ user_home_on_container }}/input_data $INPUT_DATA_PREFIX/user/{{ user_info.user_name }}/output_data
