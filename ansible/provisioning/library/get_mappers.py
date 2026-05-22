10b375cd942e10dfa8a6f052220940a23139034f
Óscar Castellanos Rodríguez <oscar.castellanos@udc.es>
2026-05-22 17:31:20 +0200

    Get mappers algorithm for DistCP updated to approximate mappers to the maximum number of concurrent maps

--- ansible/provisioning/library/get_mappers.py
+++ ansible/provisioning/library/get_mappers.py
@@ -1,15 +1,16 @@
 import math
+import sys
 from ansible.module_utils.basic import AnsibleModule
 
 DEFAULT_BLOCKS_PER_CHUNKS = 0
 
-def get_mappers(blocks, files, datanodes, mode):
+def get_mappers(blocks, files, max_mappers, mode):
 
     """
     Args:
         blocks (int): total number of block of the path to transfer
         files (int): total number of files inside the path to transfer
-        datanodes (int): total number of datanodes that will run the transfer
+        max_mappers (int): max number of concurrent mappers that will run the transfer
         mode (string): mode of modulating the algorithm
 
     Returns:
@@ -18,24 +19,24 @@ def get_mappers(blocks, files, datanodes, mode):
     Algorithm explanation:
         1. Edge cases:
             - If no files or blocks are provided return the default distcp slicing (which produces no slicing -> 1 mapper per file)
-            - If there are less or an equal number of blocks that datanodes return 1 (the lowest slicing --> 1 mapper per block);
+            - If there are less or an equal number of blocks than max_mappers return 1 (the lowest slicing --> 1 mapper per block);
 
         2. Mode-specific behaviour
             - distcp-default: always return the default distcp slicing
             - mapred-default: always return 1
-            - datanode-mapping: always try to approximate the number of mappers to the number of datanodes
+            - nearest-multiple: try to approximate the number of mappers to a multiple of max_mappers
             - best-effort: try to provide the best solution
-                - if there are less files than datanodes: run the datanode-mapping algorithm
+                - if there are less files than max_mappers: run the nearest-multiple algorithm
                 - otherwise: return the default distcp slicing
 
-        3. Details on the datanode-mapping algorithm:
+        3. Details on the nearest-multiple algorithm:
             - The number of mappers obtained by the slicing is approximated by its best-case scenario, when the blocks are evenly distributed among the files
             - The exact number of mappers could be obtained knowing the number of blocks of each file, but it can be very costly to get all of them
-            - If the amount of datanodes and files is the same: return the default distcp slicing
-            - If there are less files than datanodes: try to generate the lowest amount of mappers that are equal or greater than the amount of datanodes
-                - e.g., with 4 files, 30 blocks and 10 DNs:
+            - If the amount of max_mappers and files is the same: return the default distcp slicing
+            - If there are less files than max_mappers: try to generate the lowest amount of mappers that are equal or greater than the amount of max_mappers
+                - e.g., with 4 files, 30 blocks and 10 max_mappers:
                     - an average of 7.5 blocks per file is estimated, thus the algorithm tries until 8 blocks per file max
-                    - with no slicing:       4 mappers --> 4 < 10 -> not enough mappers for the DNs
+                    - with no slicing:       4 mappers --> 4 < 10 -> not enough mappers
                     - with 1 blockperchunk:  30 mappers
                     - with 2 blocksperchunk: 16 mappers
                     - with 3 blocksperchunk: 12 mappers
@@ -45,8 +46,8 @@ def get_mappers(blocks, files, datanodes, mode):
                     - with 7 blocksperchunk: 6 mappers
                     - with 8 blocksperchunk: 4 mappers
 
-            - If there are more files than datanodes: try to generate an amount of mappers that are as close as possible to a multiple of the number of datanodes
-                - e.g., with 12 files, 57 blocks and 5 DNs:
+            - If there are more files than max_mappers: try to generate an amount of mappers that are as close as possible to a multiple of the number of max_mappers
+                - e.g., with 12 files, 57 blocks and 5 max_mappers:
                     - an average of 5 blocks per file is estimated, thus the algorithm tries until 5 blocks per file max
                     - with no slicing:       12 mappers --> 12/5 -> 2 mappers remaining
                     - with 1 blockperchunk:  57 mappers --> 57/5 -> 2 mappers remaining
@@ -54,7 +55,7 @@ def get_mappers(blocks, files, datanodes, mode):
                     - with 3 blocksperchunk: 24 mappers --> 24/5 -> 4 mappers remaining
                     - with 4 blocksperchunk: 21 mappers --> 21/5 -> 1 mappers remaining
                     - with 5 blocksperchunk: 12 mappers --> 12/5 -> 2 mappers remaining
-                    --> the algorithm would stop here and choose 3 blocksperchunk, since with 4 remaining mappers only 1 DN would be idle
+                    --> the algorithm would stop here and choose 3 blocksperchunk, since with 4 remaining mappers only 1 mapper would be idle
     """
 
     if files <= 0: return DEFAULT_BLOCKS_PER_CHUNKS
@@ -63,22 +64,22 @@ def get_mappers(blocks, files, datanodes, mode):
     proportional_approximation = False
     if mode   == 'distcp-default': return DEFAULT_BLOCKS_PER_CHUNKS
     elif mode == 'mapred-default': return 1
-    elif mode == 'best-effort' and files >= datanodes: return DEFAULT_BLOCKS_PER_CHUNKS
-    elif mode == 'datanode-mapping':
-        if files == datanodes: return DEFAULT_BLOCKS_PER_CHUNKS
-        elif files > datanodes: proportional_approximation = True
+    elif mode == 'best-effort' and files >= max_mappers: return DEFAULT_BLOCKS_PER_CHUNKS
+    elif mode == 'nearest-multiple':
+        if files == max_mappers: return DEFAULT_BLOCKS_PER_CHUNKS
+        elif files > max_mappers: proportional_approximation = True
 
-    if blocks <= datanodes: return 1
+    if blocks <= max_mappers: return 1
 
     avg_blocks = blocks / files
 
     ## Base slicing
     best_slicing = 1
     best_estimated_mappers = blocks
-    best_diff = blocks - datanodes
-    best_remaining = best_estimated_mappers % datanodes
+    best_diff = blocks - max_mappers
+    best_remaining = best_estimated_mappers % max_mappers
 
-    ## Check if base is the best solution already when files > datanodes
+    ## Check if base is the best solution already when files > max_mappers
     if proportional_approximation and best_remaining == 0:
         print("Chosen distribution: {0} blocks per chunk --> {1} mappers minimum".format(best_slicing, best_estimated_mappers))
         return best_slicing
@@ -93,8 +94,8 @@ def get_mappers(blocks, files, datanodes, mode):
         print("{0} blocks per chunk generate at least {1} mappers".format(slicing, estimated_mappers))
 
         if not proportional_approximation:
-            ## Approximate estimated mappers to be as close as datanodes
-            diff = estimated_mappers - datanodes
+            ## Approximate estimated mappers to be as close as max_mappers
+            diff = estimated_mappers - max_mappers
             if diff == 0 or (diff > 0 and diff < best_diff):
                 best_slicing = slicing
                 best_estimated_mappers = estimated_mappers
@@ -104,8 +105,8 @@ def get_mappers(blocks, files, datanodes, mode):
                 break
 
         else:
-            ## Approximate estimated mappers to be as close as a multiple of datanodes
-            remaining = estimated_mappers % datanodes
+            ## Approximate estimated mappers to be as close as a multiple of max_mappers
+            remaining = estimated_mappers % max_mappers
             if remaining == 0 or (remaining > best_remaining):
                 best_slicing = slicing
                 best_estimated_mappers = estimated_mappers
@@ -122,19 +123,29 @@ def main():
     module_args = dict(
         number_of_blocks=dict(type='int', required=True),
         number_of_files=dict(type='int', required=True),
-        number_of_datanodes=dict(type='int', required=True),
+        max_mappers=dict(type='int', required=True),
         mode=dict(type='str', required=True),
     )
 
     module = AnsibleModule(argument_spec=module_args)
     number_of_blocks = module.params['number_of_blocks']
     number_of_files = module.params['number_of_files']
-    number_of_datanodes = module.params['number_of_datanodes']
+    max_mappers = module.params['max_mappers']
     mode = module.params['mode']
 
-    result = get_mappers(number_of_blocks, number_of_files, number_of_datanodes, mode)
+    result = get_mappers(number_of_blocks, number_of_files, max_mappers, mode)
 
     module.exit_json(changed=False, mappers=result)
 
+def main_cmd():
+    number_of_blocks = int(sys.argv[1])
+    number_of_files  = int(sys.argv[2])
+    max_mappers      = int(sys.argv[3])
+    mode             = sys.argv[4]
+
+    result = get_mappers(number_of_blocks, number_of_files, max_mappers, mode)
+    print(f"Result: {result} ")
+
 if __name__ == '__main__':
     main()
+    #main_cmd() ## useful for testing without Ansible
