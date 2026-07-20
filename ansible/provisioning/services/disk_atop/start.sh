015af4176aae16c109e0a4ad1ca190e85407fcc5
Óscar Castellanos Rodríguez <oscar.castellanos@udc.es>
2026-07-20 20:27:59 +0200

    Check OpenTSDB availability before running DiskAtop service

--- ansible/provisioning/services/disk_atop/start.sh
+++ ansible/provisioning/services/disk_atop/start.sh
@@ -2,7 +2,15 @@
 INTERVAL=5
 export PYTHONUNBUFFERED="yes"
 export POST_DOC_BUFFER_TIMEOUT=0.2
-export POST_ENDPOINT_PATH="http://{{ opentsdb_url }}:{{ opentsdb_port }}/api/put"
+ENDPOINT_PATH="http://{{ opentsdb_url }}:{{ opentsdb_port }}"
+export POST_ENDPOINT_PATH="${ENDPOINT_PATH}/api/put"
+RETRY_INTERVAL=5
+
+echo "Verifying OpenTSDB availability..."
+while ! curl -s --head --request GET "${ENDPOINT_PATH}" | grep "200" > /dev/null; do
+    echo "OpenTSDB is down. Retrying in ${RETRY_INTERVAL} seconds..."
+    sleep ${RETRY_INTERVAL}
+done
 
 source "{{ bdwatchdog_source_path }}/set_pythonpath.sh"
 
